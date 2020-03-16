70f2346fe096dce4baf20e5b8a52eaa9eaa8f740
prasanna ramesh <prasannaramesh1994@gmail.com>
2020-03-16 11:48:25 +0100

    First commit

---
+++ Sping Boot/visualizer/src/main/java/com/ovgu/vis/visualizer/MedicalVisApplication.java
@@ -0,0 +1,13 @@
+package com.ovgu.vis.visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalVisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalVisApplication.class, args);
+	}
+
+}
---
+++ Sping Boot/visualizer/src/test/java/com/ovgu/vis/visualizer/MedicalVisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovgu.vis.visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalVisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
