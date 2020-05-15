f7f6c78c7dfe10341612af4bbcb6adcd28a8a167
Prasanna Ramesh <prasannaramesh1994@gmail.com>
2020-05-15 18:57:33 +0200

    Removal of setters from entity

--- Sping Boot/visualizer/src/main/java/com/ovgu/vis/visualizer/Entity/LegendDetails.java
+++ Sping Boot/visualizer/src/main/java/com/ovgu/vis/visualizer/Entity/LegendDetails.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
 public class LegendDetails {
 
--- Sping Boot/visualizer/src/main/java/com/ovgu/vis/visualizer/Entity/PatientInfo.java
+++ Sping Boot/visualizer/src/main/java/com/ovgu/vis/visualizer/Entity/PatientInfo.java
@@ -9,9 +9,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@ToString
+
 @Entity
 public class PatientInfo {
 
